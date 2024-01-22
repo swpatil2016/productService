9d2d64f86ada3503594da0bb966828822d8ff334
swapnil <sp1791220@gmail.com>
2024-01-22 19:19:54 +0530

    RestTemplate object created by spring

--- src/main/java/com/swpatil/productService/Services/FakeStoreProductService.java
+++ src/main/java/com/swpatil/productService/Services/FakeStoreProductService.java
@@ -1,13 +1,23 @@
 package com.swpatil.productService.Services;
 
 import com.swpatil.productService.dtos.GenericProductDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service("FakeStoreProductService")
 public class FakeStoreProductService implements ProductService {
+    RestTemplateBuilder restTemplateBuilder;
+
+    @Autowired
+    public FakeStoreProductService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
     @Override
     public GenericProductDto getProductById(Long id) {
-        System.out.println("calling from Fake product");
+        RestTemplate restTemplate = restTemplateBuilder.build();
         return null;
     }
 }
