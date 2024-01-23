c16f72d4632e9072636608f3001557ab3cd1e123
swapnil <sp1791220@gmail.com>
2024-01-23 22:34:25 +0530

    make default Qualifier

--- src/main/java/com/swpatil/productService/Services/FakeStoreProductService.java
+++ src/main/java/com/swpatil/productService/Services/FakeStoreProductService.java
@@ -5,6 +5,7 @@ import com.swpatil.productService.ThirdPartyClient.FakeStoreProductServiceClient
 import com.swpatil.productService.dtos.GenericProductDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Primary
 @Service("FakeStoreProductService")
 public class FakeStoreProductService implements ProductService {
     FakeStoreProductServiceClient fakeStoreProductServiceClient;
